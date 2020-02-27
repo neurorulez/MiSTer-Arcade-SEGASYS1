738f1b575965d390ac90168bac031a06f962d2da
NeuroRulez <neuro_999@hotmail.com>
2020-02-27 15:52:00 +0100

    DB15 OSD

--- Arcade-SEGASYS1.sv
+++ Arcade-SEGASYS1.sv
@@ -67,6 +67,7 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
+    output  	  USER_OSD,	
     output	      USER_MODE,
 	input   [6:0] USER_IN,
 	output  [6:0] USER_OUT
@@ -78,6 +79,7 @@ wire   JOY_CLK, JOY_LOAD;
 wire   JOY_DATA  = USER_IN[5];
 assign USER_OUT  = |status[31:30] ? {5'b11111,JOY_CLK,JOY_LOAD} : '1;
 assign USER_MODE = |status[31:30] ;
+assign USER_OSD  = joydb15_1[8] & joydb15_1[6];
 
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
@@ -183,6 +185,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ioctl_dout(ioctl_dout),
 	.ioctl_index(ioctl_index),
 
+	.joy_raw(joydb15_1[5:0]),
 	.joystick_0(joystk1_USB),
 	.joystick_1(joystk2_USB),
 	.joystick_analog_0(joystick_analog_0),
